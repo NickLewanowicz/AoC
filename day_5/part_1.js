965c432216ae28d9b0e25977ff0c5d0ec83f4dcd
Nicholas Lewanowicz <nicklewanowicz@gmail.com>
2019-12-22 19:11:01 -0500

    Day 5 part 2

---
+++ day_5/part_1.js
@@ -0,0 +1,32 @@
+var {run} = require("../utilities");
+var fs = require("fs");
+var text = fs.readFileSync("./part_1.txt", "utf-8");
+var opcodes = text.split(",")
+
+function prepOpcode(opcodes) {
+    opcodes = opcodes.map(el => Number(el))
+    opcodes[1] = 12
+    opcodes[2] = 2
+    return opcodes
+}
+
+function findHaltValue(opcodes) {
+    let i = 0
+    while(opcodes[i] != 99) {
+        if (opcodes[i] === 1) {
+            opcodes[opcodes[i+3]] = opcodes[opcodes[i+1]] + opcodes[opcodes[i+2]]
+        } else if (opcodes[i] === 2) {
+            opcodes[opcodes[i+3]] = opcodes[opcodes[i+1]] * opcodes[opcodes[i+2]]
+        } else if (opcodes[i] === 3) {
+            //Do thing
+        }  else if (opcodes[i] === 4) {
+            //Do thing
+        } 
+        i+=4
+    }
+    return opcodes[0]
+}
+
+
+
+run(()=>findHaltValue(prepOpcode(opcodes)))
